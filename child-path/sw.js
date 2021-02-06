5fffb5ce8922435ddd8e627ff18aca5eac8b1512
Tyler Johnson <mail@tyler-johnson.ca>
2021-02-06 12:39:45 -0500

    Clarify which sw is handling fetch

--- child-path/sw.js
+++ child-path/sw.js
@@ -2,5 +2,5 @@ addEventListener("install", ()=>console.log("installed sw"));
 addEventListener("activate", ()=>console.log("activated sw"));
 
 addEventListener("fetch", event=>{
-  console.log(`handling fetch of ${event.request.url} for ${event.clientId}`);
+  console.log(`child path sw handling fetch of ${event.request.url} for ${event.clientId}`);
 });
--- sw.js
+++ sw.js
@@ -2,5 +2,5 @@ addEventListener("install", ()=>console.log("installed sw"));
 addEventListener("activate", ()=>console.log("activated sw"));
 
 addEventListener("fetch", event=>{
-  console.log(`handling fetch of ${event.request.url} for ${event.clientId}`);
+  console.log(`main sw handling fetch of ${event.request.url} for ${event.clientId}`);
 });
