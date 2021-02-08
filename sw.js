a3bb86fba7b7314083a7167b49e434caab06c6d4
Tyler Johnson <mail@tyler-johnson.ca>
2021-02-08 12:47:18 -0500

    Add a second video demonstrating SW intercept
    File Consumers can have their src set to /bucket/path/to/file and the
    service worker can intercept that request to respond with the cached
    data provided by the File Observer.

--- sw.js
+++ sw.js
@@ -3,4 +3,12 @@ addEventListener("activate", ()=>console.log("activated sw"));
 
 addEventListener("fetch", event=>{
   console.log(`main sw handling fetch of ${event.request.url} for ${event.clientId}`);
-});
+
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/test-bucket-tyler")) {
+    event.respondWith(
+      caches.open("test-cache")
+      .then(cache=>cache.match(url.pathname))
+    );
+  }
+})
