1b35f4141df14d610216b48f0e5fdee59b03cf10
Tyler Johnson <mail@tyler-johnson.ca>
2021-02-11 16:08:34 -0500

    Use child path service worker as messaging target
    Since shared workers are not well supported on Safari, use the SW
    directly for postMessage, even though it is not the service worker
    that is active for the iframe. Confirmed working.

--- child-path/sw.js
+++ child-path/sw.js
@@ -1,6 +1,34 @@
+const remoteFilePath = "https://storage.googleapis.com/storage/v1/b/BUCKET/o/OBJECT?alt=media";
+
 addEventListener("install", ()=>console.log("installed sw"));
 addEventListener("activate", ()=>console.log("activated sw"));
+
+onmessage = event=>{
+  console.log("Child path service worker received message", event);
+
+  if (!event.data) {return;}
+  if (event.data.filePath) {return downloadAndCache(event);}
+};
 
 addEventListener("fetch", event=>{
   console.log(`child path sw handling fetch of ${event.request.url} for ${event.clientId}`);
 });
+
+function downloadAndCache(event) {
+  const fileConsumer = event.source || event.srcElement;
+
+  const {filePath} = event.data;
+  if (filePath[0] !== "/") {return fileConsumer.postMessage({error: "invalid path"});}
+
+  const [bucket, ...objectPathElements] = filePath.split("/").slice(1);
+
+  const contentUrl = remoteFilePath.replace("BUCKET", bucket)
+  .replace("OBJECT", objectPathElements.join("/"));
+
+  fetch(contentUrl).then(resp=>{
+    console.log("caching content");
+    caches.open("test-cache")
+    .then(cache=>cache.put(["", bucket, ...objectPathElements].join("/"), resp))
+    .then(()=>fileConsumer.postMessage({filePath}));
+  });
+}
--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
 addEventListener("install", ()=>console.log("installed sw"));
 addEventListener("activate", ()=>console.log("activated sw"));
 
+onmessage = event=>{
+  console.log("Service worker received message", event);
+  event.source.postMessage("Hello from root path sw");
+};
+
 addEventListener("fetch", event=>{
   console.log(`main sw handling fetch of ${event.request.url} for ${event.clientId}`);
 
