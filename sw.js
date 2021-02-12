c246d8a7fc46c297aa37e1456e4f14e5d58bda43
Tyler Johnson <mail@tyler-johnson.ca>
2021-02-12 06:23:07 -0500

    Revert "Fix safari?"
    This reverts commit 5818cd898c61719ab5a567de19da35f74f30a46a.
    Did not fix.

--- sw.js
+++ sw.js
@@ -8,11 +8,10 @@ onmessage = event=>{
 };
 
 addEventListener("fetch", event=>{
-  const pathname = new URL(event.request.url).pathname;
+  console.log(`main sw handling fetch of ${event.request.url} for ${event.clientId}`);
 
-  event.respondWith(
-    pathname.startsWith("/test-bucket-tyler") ? 
-      caches.match(pathname) :
-      fetch(event.request)
-  );
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/test-bucket-tyler")) {
+    event.respondWith(caches.match(url.pathname));
+  }
 })
