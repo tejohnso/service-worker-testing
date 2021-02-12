68163799c9c77f320cea60fd3bc22bfdb7c17491
Tyler Johnson <mail@tyler-johnson.ca>
2021-02-12 05:41:36 -0500

    Use caches.match

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-let cacheStorage;
-
 addEventListener("install", event=>{
   console.log("installing main sw");
   event.waitUntil(caches.open("test-cache").then(cache=>cacheStorage = cache));
@@ -17,6 +15,6 @@ addEventListener("fetch", event=>{
 
   const url = new URL(event.request.url);
   if (url.pathname.startsWith("/test-bucket-tyler")) {
-    event.respondWith(cacheStorage.match(url.pathname));
+    event.respondWith(caches.match(url.pathname));
   }
 })
